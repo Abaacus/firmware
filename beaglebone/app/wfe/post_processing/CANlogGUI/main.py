2c9a10ce2e2c301da8fdee5e0f0c40c7d3492ec4
Jerry Hu <j376hu@uwaterloo.ca>
2022-10-10 10:36:51 -0400

    Add CanlogGUI folder under beaglebone/app/wfe/post_processing

---
+++ beaglebone/app/wfe/post_processing/CANlogGUI/main.py
@@ -0,0 +1,102 @@
+
+import json
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtWidgets import QMessageBox
+
+from dependencies.UI.GUI import Ui_Dialog
+from dependencies.Graph import *
+
+def log():
+     print("GUI running")
+
+def error(message = "Error: Please check your input"):
+     msg = QMessageBox()
+     msg.setText(message)
+     msg.setWindowTitle("Error")
+     msg.exec_()
+
+def showGraph():
+     print("log")
+     dbcFile = ui.DBCFile.text()
+     if (ui.FileLocation.toPlainText() != ''):
+          data_dict = json.loads(logToJsonDict(ui.FileLocation.toPlainText(), dbcFile))
+     else:
+          error("Please select a file to graph")
+          return
+     #save all the checked signal in list which is named checked_list 
+     checked_list = []
+     root = ui.CANSignals.invisibleRootItem()
+     all_signals = root.child(0)
+     signal_count = root.child(0).childCount()
+     for i in range(signal_count):
+          child = all_signals.child(i)
+          if (child.checkState(0) == QtCore.Qt.Checked):
+               checked_list.append(child.text(0))
+     # print(checked_list)
+
+     # in data_dict, if signal name is selected, save into checked_data_dict (filtering)
+     checked_data_dict = {}
+     for i in data_dict:
+          if (i in checked_list):
+               checked_data_dict[i] = data_dict[i]
+     # print(checked_data_dict)
+
+     #obtain all other constraints and save in args_dict
+     args_dict = {}
+     args_dict["RegexInput"] = ui.RegexInput.text()
+     args_dict["MaxXInput"] = ui.MaxXInput.text()
+     args_dict["MinXInput"] = ui.MinXInput.text()
+     args_dict["MaxYInput"] = ui.MaxYInput.text()
+     args_dict["MinYInput"] = ui.MinYInput.text()
+     # print(args_dict)
+
+     graph(checked_data_dict, args_dict)
+
+     
+
+def getSignalNames():
+     if (ui.FileLocation.toPlainText() != ''):
+          data_dict = json.loads(logToJsonDict(ui.FileLocation.toPlainText(), "dependencies/2018CAR.dbc"))
+     signalNames = []
+     for key in data_dict.keys():
+          if(signalNames.count(key) == 0):
+               signalNames.append(key)
+     signalNames.sort()
+     return signalNames
+
+def loadExtraUiLogic(ui):
+     ui.SelectFile.clicked.connect(selectFile)
+     ui.GraphBtn.clicked.connect(showGraph)
+
+def selectFile():
+     print(ui.RegexInput.text())
+     fileLocation = QFileDialog.getOpenFileName(None, "Select file", "Log file (.log)", "*.log")[0]
+     if fileLocation != "":
+          ui.FileLocation.setText(fileLocation)
+          # graph(ui.FileLocation.toPlainText(), None)
+     loadSignalList(ui.CANSignals, getSignalNames())
+
+def loadSignalList(treeWidget, data_list):
+     treeWidget.clear()
+     root = QtWidgets.QTreeWidgetItem(treeWidget)
+     root.setText(0, "All")
+     root.setFlags(root.flags() | QtCore.Qt.ItemIsTristate | QtCore.Qt.ItemIsUserCheckable)
+     for x in range(len(data_list)):
+          signal = QtWidgets.QTreeWidgetItem(root)
+          signal.setFlags(signal.flags() | QtCore.Qt.ItemIsUserCheckable)
+          signal.setText(0, data_list[x])
+          signal.setCheckState(0, QtCore.Qt.Unchecked)
+     
+if __name__ == "__main__":
+     app = QtWidgets.QApplication(sys.argv)
+     MainWindow = QtWidgets.QWidget()
+     ui = Ui_Dialog()
+     ui.setupUi(MainWindow)
+     loadExtraUiLogic(ui)
+     MainWindow.show()
+     sys.exit(app.exec_())
+
+
+
