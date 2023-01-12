e9871976d87d5fd1ac0cf263cb71f2f714c0aaf8
Daniel Bishara <d2bishar@uwaterloo.ca>
2023-01-12 00:04:34 -0500

    Added task to check motor temp

---
+++ pdu/Inc/cooling.h
@@ -0,0 +1,10 @@
+#ifndef cooling_H
+#define cooling_H
+#include "stm32f7xx_hal.h"
+#include "stdbool.h"
+
+#define COOLING_TASK_INTERVAL_MS 5000
+#define COOLING_THRESHOLD 24
+#define COOLING_TASK_ID 6
+
+#endif
---
+++ pdu/Src/cooling.c
@@ -0,0 +1,49 @@
+#include "cooling.h"
+#include "bsp.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "state_machine.h"
+#include "controlStateMachine.h"
+#include "FreeRTOS_CLI.h"
+#include "debug.h"
+#include "adc.h"
+#include <stdbool.h>
+#include "pdu_can.h"
+#include "watchdog.h"
+#include "canReceive.h"
+#include "pdu_dtc.h"
+
+void coolingTask(void *pvParameters)
+{
+     if (registerTaskToWatch(COOLING_TASK_ID, COOLING_TASK_INTERVAL_MS, false, NULL) != HAL_OK)
+    {
+        ERROR_PRINT("Failed to register cooling task with watchdog!\n");
+        Error_Handler();
+    }
+
+    // Delay to allow system to turn on
+    vTaskDelay(100);
+
+    TickType_t xLastWakeTime = xTaskGetTickCount();
+    while(1)
+    {
+        int tempMRight = TempMotorRight;
+        int tempMLeft = TempMotorLeft;
+
+        if(tempMRight>COOLING_THRESHOLD || tempMLeft>COOLING_THRESHOLD){
+            if(fsmGetState(&coolingFsmHandle) != COOL_STATE_ON){
+                fsmSendEvent(&coolingFsmHandle, COOL_EV_OVERTEMP_WARNING, portMAX_DELAY);
+            }
+        }
+        else{
+            if(fsmGetState(&coolingFsmHandle) == COOL_STATE_ON){
+                fsmSendEvent(&coolingFsmHandle, COOL_EV_OVERTEMP_WARNING, portMAX_DELAY);
+            }
+        }
+
+        watchdogTaskCheckIn(COOLING_TASK_ID);
+        vTaskDelayUntil(&xLastWakeTime, COOLING_TASK_INTERVAL_MS);
+    }
+
+
+}
