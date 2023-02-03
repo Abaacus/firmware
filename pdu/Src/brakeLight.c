e0f57c640399cf80e1d8c062ac200d5aba8c6b1d
agajelli@uwaterloo.ca <argajelli@gmail.com>
2023-02-03 17:20:08 -0500

    removed redundant var in isBrakePressed

--- pdu/Src/brakeLight.c
+++ pdu/Src/brakeLight.c
@@ -7,14 +7,14 @@
 
 #define BRAKE_TASK_PERIOD_MS 300
 
-bool isBrakePressed(uint32_t brakePercent)
+bool isBrakePressed()
 {
-    return (BrakePercent > BrakelightThresholdPercent);
+    return BrakePercent > BrakelightThresholdPercent;
 }
 
 void CAN_Msg_VCU_Data_Callback()
 {
-    if (isBrakePressed(BrakePercent)) {
+    if (isBrakePressed()) {
         BRAKE_LIGHT_ENABLE;
     } else {
         BRAKE_LIGHT_DISABLE;
