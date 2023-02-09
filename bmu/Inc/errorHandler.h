03c1b41f25050e8f5a21cfc038f8f2728426ecd8
Kieran Hulsman <kghulsma@uwaterloo.ca>
2023-02-08 20:25:53 -0500

    removed unnessesary enum entries

--- bmu/Inc/errorHandler.h
+++ bmu/Inc/errorHandler.h
@@ -14,36 +14,17 @@ typedef enum Error_events_t
     Failed_initPackVoltageQueue,                          // 6
     Failed_init_imd_measurement,                          // 7
     Failed_init_HW_check_timer,                           // 8
-    Failed_ADC_Init,                                      // 9
-    Failed_ADC_ConfigChannel,                             // 10
-    Failed_DMA_Init,                                      // 11
-    Failed_CAN_Init,                                      // 12
-    Failed_IWDG_Init,                                     // 13
-    Failed_RCC_OscConfig,                                 // 14
-    Failed_PWREx_EnableOverDrive,                         // 15
-    Failed_RCC_ClockConfig,                               // 16   
-    Failed_RCCEx_PeriphCLKConfig,                         // 17
-    Failed_SPI_Init,                                      // 18
-    Failed_TIM_Base_Init,                                 // 19
-    Failed_TIM_ConfigClockSource,                         // 20
-    Failed_TIM_PWM_Init,                                  // 21
-    Failed_TIMEx_MasterConfigSynchronization,             // 22
-    Failed_TIM_PWM_ConfigChannel,                         // 23
-    Failed_TIM_IC_Init,                                   // 24
-    Failed_TIM_SlaveConfigSynchro,                        // 25
-    Failed_TIM_IC_ConfigChannel,                          // 26
-    Failed_UART_Init,                                     // 27
-    Failed_HV_Measure_Task_Unregistered_with_Watchdog,    // 28
-    Failed_IMD_Measurement_not_Started,                   // 29
-    Failed_IMD_Task_Unregistered_with_Watchdog,           // 30
-    Failed_initVoltageAndTempArrays,                      // 31
-    Failed_Battery_Task_Unregistered_with_Watchdog,       // 32
-    Failed_canStart,                                      // 33
-    Failed_Fault_Monitor_Task_Unregistered_with_Watchdog, // 34
-    Failed_pcdcInit,                                      // 35
-    Failed_Brake_Sensor_ADC_DMA_Conversions_not_Started,  // 36
-    Failed_sensorTaskInit,                                // 37
-    Failed_fanInit                                        // 38
+    Failed_HV_Measure_Task_Unregistered_with_Watchdog,    // 9
+    Failed_IMD_Measurement_not_Started,                   // 10
+    Failed_IMD_Task_Unregistered_with_Watchdog,           // 11
+    Failed_initVoltageAndTempArrays,                      // 12
+    Failed_Battery_Task_Unregistered_with_Watchdog,       // 13
+    Failed_canStart,                                      // 14
+    Failed_Fault_Monitor_Task_Unregistered_with_Watchdog, // 15
+    Failed_pcdcInit,                                      // 16
+    Failed_Brake_Sensor_ADC_DMA_Conversions_not_Started,  // 17
+    Failed_sensorTaskInit,                                // 18
+    Failed_fanInit                                        // 19
 } Error_events_t;
 
 void BMU_error(uint8_t error_num);
