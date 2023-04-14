9b41a9b44623bb80eed2bf80b79297beb9c34d87
I am the Bay Laptop <uwaterlooelectric@gmail.com>
2023-04-14 16:02:36 -0400

    BMU Retransmission

--- bmu/Cube-F7-Src-respin/Src/can.c
+++ bmu/Cube-F7-Src-respin/Src/can.c
@@ -6,7 +6,7 @@
   ******************************************************************************
   * @attention
   *
-  * <h2><center>&copy; Copyright (c) 2022 STMicroelectronics.
+  * <h2><center>&copy; Copyright (c) 2023 STMicroelectronics.
   * All rights reserved.</center></h2>
   *
   * This software component is licensed by ST under Ultimate Liberty license
@@ -79,7 +79,7 @@ void MX_CAN3_Init(void)
   hcan3.Init.TimeTriggeredMode = DISABLE;
   hcan3.Init.AutoBusOff = DISABLE;
   hcan3.Init.AutoWakeUp = DISABLE;
-  hcan3.Init.AutoRetransmission = DISABLE;
+  hcan3.Init.AutoRetransmission = ENABLE;
   hcan3.Init.ReceiveFifoLocked = DISABLE;
   hcan3.Init.TransmitFifoPriority = DISABLE;
   if (HAL_CAN_Init(&hcan3) != HAL_OK)
