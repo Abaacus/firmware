8a3ea4714e2ce215aeabf54ceb43159a2e778327
I am the Bay Laptop <uwaterlooelectric@gmail.com>
2023-04-21 21:38:08 -0400

    Resync jump width + better 75% sample point for CAN

--- bmu/Cube-F7-Src-respin/Src/can.c
+++ bmu/Cube-F7-Src-respin/Src/can.c
@@ -71,11 +71,11 @@ void MX_CAN3_Init(void)
 
   /* USER CODE END CAN3_Init 1 */
   hcan3.Instance = CAN3;
-  hcan3.Init.Prescaler = 10;
+  hcan3.Init.Prescaler = 5;
   hcan3.Init.Mode = CAN_MODE_NORMAL;
   hcan3.Init.SyncJumpWidth = CAN_SJW_1TQ;
-  hcan3.Init.TimeSeg1 = CAN_BS1_8TQ;
-  hcan3.Init.TimeSeg2 = CAN_BS2_1TQ;
+  hcan3.Init.TimeSeg1 = CAN_BS1_13TQ;
+  hcan3.Init.TimeSeg2 = CAN_BS2_6TQ;
   hcan3.Init.TimeTriggeredMode = DISABLE;
   hcan3.Init.AutoBusOff = ENABLE;
   hcan3.Init.AutoWakeUp = ENABLE;
