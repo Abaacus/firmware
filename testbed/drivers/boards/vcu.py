a71d574314572fc211c49f7001ff57436dc75d6b
agajelli@uwaterloo.ca <argajelli@gmail.com>
2023-07-08 19:04:00 -0400

    Removed boards and hil_boards folder

--- testbed/drivers/boards/vcu.py
+++
@@ -1,5 +0,0 @@
-from drivers.common_drivers.can_driver import VehicleBoard
-class VCU(VehicleBoard):
-    CAN_ID = 2
-    def __init__(self, name, bus, db, listener):
-        super().__init__(name, bus, db, listener, self.CAN_ID)
--- testbed/drivers/hil_boards/vcu_hil.py
+++
@@ -1,6 +0,0 @@
-from drivers.common_drivers.can_driver import HILBoard
-class VCUHil(HILBoard):
-    CAN_ID = 7
-    def __init__(self, name, bus, db, listener):
-        super().__init__(name, bus, db, listener, self.CAN_ID)
-        
