c60fb467071be36e367564adfc6d06cadae6ef43
Jacky Lim <cllim@uwaterloo.ca>
2023-07-10 03:09:38 +0000

    improve test names and move description to inside the function so that it would show up in pytest

--- testbed/tests/torqueVectoring_test.py
+++ testbed/tests/torqueVectoring_test.py
@@ -11,8 +11,8 @@ vcu: VehicleBoard = teststand.vehicle_boards["vcu"]
 sleepDurationMs = 0.2
 tolerance = 5
 
-# setup the car configuration for the tests
 def test_TV_setup(teststand):
+    # setup the car configuration for the tests
     print(teststand.hil_boards)
     
     # set 50% throttle
@@ -26,19 +26,21 @@ def test_TV_setup(teststand):
     time.sleep(sleepDurationMs)
     assert vcu_hil.get_signal("Throttle_B_status")
 
-# test 1: 50% throttle and steering angle in dead zone (0 degree)
-def test_TV_1(teststand):
+
+def test_TV_deadzone(teststand):
+    # test 1: 50% throttle and steering angle in dead zone (0 degree)
     vcu_hil.set_signal("Steering_raw", {"Steering_raw": 1650})
     time.sleep(sleepDurationMs)
     assert vcu_hil.get_signal("Steering_status")
-    
+
     torqueDemandR = vcu.get_signal("TorqueDemandRight")
     torqueDemandL = vcu.get_signal("TorqueDemandLeft")
     assert torqueDemandR == torqueDemandL,\
     f"torqueDemandR <{torqueDemandR}>, torqueDemandL <{torqueDemandL}>"
 
-# test 2: 50% throttle and steering fully right
-def test_TV_2(teststand):
+
+def test_TV_100R(teststand):
+    # test 2: 50% throttle and steering fully right (100 degrees)
     vcu_hil.set_signal("Steering_raw", {"Steering_raw": 3300})
     time.sleep(sleepDurationMs)
     assert vcu_hil.get_signal("Steering_status")
@@ -50,8 +52,9 @@ def test_TV_2(teststand):
     assert math.isclose(torqueDemandL, expectedTL = 27.50, abs_tol = tolerance),\
     f"torqueDemandR <{torqueDemandR}> exceeds tolerance <{tolerance}>"
 
-# test 3: 50% throttle and steering fully left
-def test_TV_3(teststand):
+
+def test_TV_100L(teststand):
+    # test 3: 50% throttle and steering fully left (100 degrees)
     vcu_hil.set_signal("Steering_raw", {"Steering_raw": 0})
     time.sleep(sleepDurationMs)
     assert vcu_hil.get_signal("Steering_status")
@@ -63,8 +66,9 @@ def test_TV_3(teststand):
     assert math.isclose(torqueDemandL, expectedTL = 2.50, abs_tol = tolerance),\
     f"torqueDemandR <{torqueDemandR}> exceeds tolerance <{tolerance}>"
 
-# test 4: 50% throttle and steering 50 degrees right
-def test_TV_4(teststand):
+
+def test_TV_50R(teststand):
+    # test 4: 50% throttle and steering 50 degrees right
     vcu_hil.set_signal("Steering_raw", {"Steering_raw": 2480})
     time.sleep(sleepDurationMs)
     assert vcu_hil.get_signal("Steering_status")
