f1b80a60a3c8250a54328f1cff2f3be970d32f12
Jacky Lim <cllim@uwaterloo.ca>
2023-07-10 01:49:22 +0000

    add assert error messages

--- testbed/tests/torqueVectoring_test.py
+++ testbed/tests/torqueVectoring_test.py
@@ -13,7 +13,7 @@ def test_torqueVectoring(teststand):
     throttleB_mV = 1130
     vcu_hil.set_signal("Throttle_position_A", {"Throttle_position_A": throttleA_mV})
     time.sleep(0.2)
-    assert vcu_hil.get_signal("Throttle_A_status") 
+    assert vcu_hil.get_signal("Throttle_A_status")
 
     vcu_hil.set_signal("Throttle_position_B", {"Throttle_position_B": throttleB_mV})
     time.sleep(0.2)
@@ -25,7 +25,8 @@ def test_torqueVectoring(teststand):
     assert vcu_hil.get_signal("Steering_status")
     torqueDemandR = vcu.get_signal("TorqueDemandRight")
     torqueDemandL = vcu.get_signal("TorqueDemandLeft")
-    assert torqueDemandR == torqueDemandL
+    assert torqueDemandR == torqueDemandL,\
+    f"torqueDemandR <{torqueDemandR}>, torqueDemandL <{torqueDemandL}>"
 
     # test 2: 50% throttle and steering fully right
     vcu_hil.set_signal("Steering_raw", {"Steering_raw": 3300})
@@ -35,8 +36,10 @@ def test_torqueVectoring(teststand):
     torqueDemandR = vcu.get_signal("TorqueDemandRight")
     torqueDemandL = vcu.get_signal("TorqueDemandLeft")
     tolerance = 5
-    assert math.isclose(torqueDemandR, expectedTR = 2.50, abs_tol = tolerance)
-    assert math.isclose(torqueDemandL, expectedTL = 27.50, abs_tol = tolerance)
+    assert math.isclose(torqueDemandR, expectedTR = 2.50, abs_tol = tolerance),\
+    f"torqueDemandR <{torqueDemandR}> exceeds tolerance <{tolerance}>"
+    assert math.isclose(torqueDemandL, expectedTL = 27.50, abs_tol = tolerance),\
+    f"torqueDemandR <{torqueDemandR}> exceeds tolerance <{tolerance}>"
 
     # test 3: 50% throttle and steering fully left
     vcu_hil.set_signal("Steering_raw", {"Steering_raw": 0})
@@ -45,8 +48,10 @@ def test_torqueVectoring(teststand):
 
     torqueDemandR = vcu.get_signal("TorqueDemandRight")
     torqueDemandL = vcu.get_signal("TorqueDemandLeft")
-    assert math.isclose(torqueDemandR, expectedTR = 27.50, abs_tol = tolerance)
-    assert math.isclose(torqueDemandL, expectedTL = 2.50, abs_tol = tolerance)
+    assert math.isclose(torqueDemandR, expectedTR = 27.50, abs_tol = tolerance),\
+    f"torqueDemandR <{torqueDemandR}> exceeds tolerance <{tolerance}>"
+    assert math.isclose(torqueDemandL, expectedTL = 2.50, abs_tol = tolerance),\
+    f"torqueDemandR <{torqueDemandR}> exceeds tolerance <{tolerance}>"
 
     # test 3: 50% throttle and steering 50 degrees right
     vcu_hil.set_signal("Steering_raw", {"Steering_raw": 2480})
@@ -55,5 +60,7 @@ def test_torqueVectoring(teststand):
 
     torqueDemandR = vcu.get_signal("TorqueDemandRight")
     torqueDemandL = vcu.get_signal("TorqueDemandLeft")
-    assert math.isclose(torqueDemandR, expectedTR = 10.50, abs_tol = tolerance)
-    assert math.isclose(torqueDemandL, expectedTL = 25.50, abs_tol = tolerance)
+    assert math.isclose(torqueDemandR, expectedTR = 10.50, abs_tol = tolerance),\
+    f"torqueDemandR <{torqueDemandR}> exceeds tolerance <{tolerance}>"
+    assert math.isclose(torqueDemandL, expectedTL = 25.50, abs_tol = tolerance),\
+    f"torqueDemandL <{torqueDemandL}> exceeds tolerance <{tolerance}>"
