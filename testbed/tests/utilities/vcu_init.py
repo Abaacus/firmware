57137dd0e4eae05f55a6d1cacea5ff5b34972bea
Addison <74084822+addison-ch@users.noreply.github.com>
2023-07-28 20:13:39 -0400

    commented out script

--- testbed/tests/utilities/vcu_init.py
+++ testbed/tests/utilities/vcu_init.py
@@ -1,23 +1,23 @@
-def vcuInit() -> bool:
+# def vcuInit() -> bool:
 
-    if setVoltage('Throttle_position_A',volts_to_mV(2.19)) == False:
-        # 2.19v === ~15% throttle (note- throttle A is inverted)
-        print("Throttle A not init")
-        return False
-    if setVoltage('Throttle_position_B',volts_to_mV(1.3)) == False:
-        # 1.3v === ~15% throttle
-        print("Throttle B not init")
-        return False
-    if setVoltage('Brake_position',volts_to_mV(1)) == False:
-        # brake pos > 0.956V === break is hard pressed
-        print("Brake pos not init")
-        return False
-    if setVoltage('Steering_raw',volts_to_mV(1.65)) == False:
-        print("Steering not init")
-        return False
-    if setVoltage('Brake_pres_raw',volts_to_mV(1)) == False:
-        print("Brake pressure not init")
-        return False
+#     if setVoltage('Throttle_position_A',volts_to_mV(2.19)) == False:
+#         # 2.19v === ~15% throttle (note- throttle A is inverted)
+#         print("Throttle A not init")
+#         return False
+#     if setVoltage('Throttle_position_B',volts_to_mV(1.3)) == False:
+#         # 1.3v === ~15% throttle
+#         print("Throttle B not init")
+#         return False
+#     if setVoltage('Brake_position',volts_to_mV(1)) == False:
+#         # brake pos > 0.956V === break is hard pressed
+#         print("Brake pos not init")
+#         return False
+#     if setVoltage('Steering_raw',volts_to_mV(1.65)) == False:
+#         print("Steering not init")
+#         return False
+#     if setVoltage('Brake_pres_raw',volts_to_mV(1)) == False:
+#         print("Brake pressure not init")
+#         return False
 
-    print('VCU sensors initialized successfully')
-    return True
+#     print('VCU sensors initialized successfully')
+#     return True
