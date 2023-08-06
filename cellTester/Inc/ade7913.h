e321474ba87cbb355c373f7d62b047999476468c
agajelli@uwaterloo.ca <argajelli@gmail.com>
2023-08-06 15:28:20 -0400

    User init for cell adc init

--- cellTester/Inc/ade7913.h
+++ cellTester/Inc/ade7913.h
@@ -1,7 +1,8 @@
 #ifndef ADE7913_H_
 #define ADE7913_H_
 
-HAL_StatusTypeDef hvadc_init();
+#include "stm32f0xx_hal.h"
+HAL_StatusTypeDef cell_adc_init();
 HAL_StatusTypeDef adc_read(uint8_t addr, int32_t *dataOut);
 HAL_StatusTypeDef adc_readbyte(uint8_t addr, uint8_t *dataOut);
 HAL_StatusTypeDef adc_write(uint8_t addr, uint8_t data);
@@ -9,4 +10,4 @@ HAL_StatusTypeDef adc_read_current(float *dataOut);
 HAL_StatusTypeDef adc_read_v1(float *dataOut);
 HAL_StatusTypeDef adc_read_v2(float *dataOut);
 
-#endif /* ADE7012_H_*/
+#endif /* ADE7013_H_*/
